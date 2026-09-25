5c26ac7d094ec013036ebf4e9a38b78f2d583de4
rhendytay <rhendytay06@gmail.com>
2026-09-26 01:11:35 +0700

    fix: preserve required networking behavior

--- drivers/net/loopback.c
+++ drivers/net/loopback.c
@@ -202,7 +202,7 @@ static __net_init int loopback_net_init(struct net *net)
 	if (err)
 		goto out_free_netdev;
 
-	BUG_ON(dev->ifindex != LOOPBACK_IFINDEX);
+	// BUG_ON(dev->ifindex != LOOPBACK_IFINDEX); // patched for Halium: allow non-1 ifindex in netns
 	net->loopback_dev = dev;
 	return 0;
 
